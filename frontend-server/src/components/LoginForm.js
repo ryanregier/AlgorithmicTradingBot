8195b9d2b6a2b40f5f3c02d3649037b325f54340
Steven Barker <steven.barker@my.wheaton.edu>
2021-03-06 20:42:10 -0600

    Removed name text field

--- frontend-server/src/components/LoginForm.js
+++ frontend-server/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import App from '../App';
 
 
 function LoginForm({Login, error}) {
-    const [details, setDetails] = useState({name: "", email: "", password: ""});
+    const [details, setDetails] = useState({email: "", password: ""});
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -14,10 +14,6 @@ function LoginForm({Login, error}) {
            <div className="form-inner">
                <h2>Login</h2>
                {(error != "") ? (<div className="error">{error}</div>) : ""}
-               <div className="form-group">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
-               </div>
                <div className="form-group">
                     <label htmlFor="email">Email:</label>
                     <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
