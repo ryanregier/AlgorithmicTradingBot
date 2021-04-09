3f2b04b95e75a75b8e0f80771790b44d527db2e6
ryanregier <ryan.regier@my.wheaton.edu>
2021-04-08 19:56:57 -0500

    Changes to DB system and algo

--- MovingAveragesStrategy.py
+++ MovingAveragesStrategy.py
@@ -12,6 +12,8 @@ import pandas as pd
 import numpy as np
 import time
 
+global own
+
 
 def getPrice(sym):
     start = time.time()
@@ -50,14 +52,15 @@ def getStocks():
     return VS.getTopTickers()
 
 
-def startAlgo(sym, qty=1, action="market", time_in_force='fok'):
+def startAlgo(sym, qty=10, action="market", time_in_force='fok'):
+    global own
     # alpaca.execute_trade(sym, 1, 'buy', 'market', 'fok')
     priceBought = 0
     profit = 0
     qtyOwned = 0
-    own = False
+    #own = False
     while True:
-        # time.sleep(1)
+        time.sleep(1)
         own = alpaca.portfolioHas(sym)
         data = yf.download(tickers=sym, period='1d', interval='1m')
         xVals = data.index.tolist()
@@ -76,7 +79,7 @@ def startAlgo(sym, qty=1, action="market", time_in_force='fok'):
         print(str(df['SELL'][-1]))
         if own:
             profit += qty * df['Adj Close'][-1]
-        if signal:
+        if signal and not own:
             try:
                 alpaca.execute_trade(sym, qty, 'buy', 'market', 'fok')
                 priceBought = float(getPrice(sym))
@@ -84,7 +87,7 @@ def startAlgo(sym, qty=1, action="market", time_in_force='fok'):
                 print(qtyOwned)
                 print("Price Bought: " + str(priceBought * 1.0009))
                 print("We bought shares of " + sym)
-                # alpaca.update_lastTrade(sym)
+                alpaca.update_lastTrade()
                 own = True
             except alpaca_trade_api.rest.APIError:
                 print("Insufficient buying power")
@@ -93,7 +96,7 @@ def startAlgo(sym, qty=1, action="market", time_in_force='fok'):
             alpaca.create_order(sym, qtyOwned, 'sell', action, 'fok')
             print("We sold all our shares")
             print("Profit: " + str(profit))
-            # alpaca.update_lastTrade(sym)
+            alpaca.update_lastTrade()
             qtyOwned = 0
             own = False
             profit = 0
@@ -111,7 +114,9 @@ def enterStocks(ls):
 
 def main():
     if alpaca.marketIsOpen():
-        startAlgo(sym='SNDL')
+        ticker = input("Enter the ticker to run the algo on")
+        print(ticker)
+        startAlgo(sym=ticker)
     else:
         print("Sorry, market is closed.")
         print("Try buying tomorrow!")
---
+++ Stream.py
@@ -0,0 +1,35 @@
+import json
+import websocket
+
+BASE_URL = "https://paper-api.alpaca.markets"
+API_KEY = 'PKTSOANLV7FIXQR8NJSR'
+SECRET_KEY = 'H211M4TJ7C7D8AUUuPt50IyAUh6AKTd4idPRWLZc'
+
+
+def on_open(ws):
+    print("opened")
+    auth_data = {
+        "action": "authenticate",
+        "data": {"key_id": API_KEY, "secret_key": SECRET_KEY}
+    }
+
+    ws.send(json.dumps(auth_data))
+
+    listen_message = {"action": "listen", "data": {"streams": ["AM.TSLA"]}}
+
+    ws.send(json.dumps(listen_message))
+
+
+def on_message(ws, message):
+    print("received a message")
+    print(message)
+
+
+def on_close(ws):
+    print("closed connection")
+
+
+socket = "wss://data.alpaca.markets/stream"
+
+ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
+ws.run_forever()
